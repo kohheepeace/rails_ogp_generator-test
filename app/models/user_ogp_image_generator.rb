d0cd161768b9ed1a9cbb6329808165d2b4bdc560
rizki113070213 <rizki113070213@gmail.com>
2019-04-02 17:02:03 +0700

    Added initialize method and updated generate and make_circle_mask method

--- app/models/user_ogp_image_generator.rb
+++ app/models/user_ogp_image_generator.rb
@@ -1,12 +1,22 @@
 class UserOgpImageGenerator
   include Magick
   require 'base64'
+  require 'word_wrap/core_ext'
+
+  def initialize(id="", name="", job="", bio="")
+    @id = id.present? ? id : 1
+    @name = name.present? ? name : "Della Zemlak"
+    @job = job.present? ? job : "Amazon inc CEOです"
+    @bio = bio.present? ? bio : "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like. This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+  end
 
   def generate
-    user_id = 1
-    user_name = "Della Zemlak"
-    user_job = "Amazon inc CEOです"
-    user_bio = "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like. This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+    user_id = @id
+    user_name = @name
+    user_job = @job
+    user_bio = @bio
+
+    user_bio = user_bio.fit 50
 
     image = Magick::ImageList.new
 
@@ -19,35 +29,43 @@ class UserOgpImageGenerator
     # draw text config
     draw = Magick::Draw.new
     draw.gravity = Magick::CenterGravity
-    draw.font = Rails.root.join('app', 'assets', 'fonts', 'NotoSansCJKjp-Medium.otf').to_s
+    # draw.font = Rails.root.join('app', 'assets', 'fonts', 'NotoSansCJKjp-Medium.otf').to_s
+    draw.font = "Noto-Sans-CJK-JP-Medium"
     draw.fill = '#000'
 
     # avatar
     avatar_image = Magick::Image.from_blob(open("https://pbs.twimg.com/profile_images/1105663166334230529/6M_HE8S7_400x400.jpg").read).first
-    avatar_image = avatar_image.resize(320, 320)
-    avatar_image = make_circle_mask(avatar_image, 320)
-    image.composite!(avatar_image, Magick::CenterGravity, -380, -100, Magick::OverCompositeOp)
+    avatar_image = avatar_image.resize(300, 300)
+    avatar_image = make_circle_mask(avatar_image, 300)
+    image.composite!(avatar_image, Magick::CenterGravity, -380, -111, Magick::OverCompositeOp)
 
     # write name to canvas
     if user_name.present?
-      draw.pointsize = 58
-      draw.annotate(image, 0, 0, -60, -240, user_name) {
+      draw.pointsize = 60
+      draw.annotate(image, 0, 0, 417, 55, user_name) {
         self.fill = '#000'
+        self.gravity = Magick::NorthWestGravity
       }
     end
 
     # write job title to canvas
     if user_job.present?
-      draw.pointsize = 30
-      draw.annotate(image, 0, 0, -60, -180, user_job) {
+      draw.pointsize = 27
+      draw.annotate(image, 0, 0, 423, 130, user_job) {
         self.fill = 'rgba(0, 0, 0, 0.54)'
+        self.gravity = Magick::NorthWestGravity
       }
     end
 
     # write bio to canvas
     if user_bio.present?
-      draw.pointsize = 40
-      draw.annotate(image, 0, 0, -60, -140, user_bio) {
+      draw.pointsize = 35
+      draw.annotate(image, 0, 0, 423, 200, user_bio) {
+        self.font = "Noto-Sans-CJK-JP-Regular"
+        self.gravity = Magick::NorthWestGravity
+        self.kerning = -2
+        self.interword_spacing = 12
+        self.interline_spacing = -5
         self.fill = '#000'
       }
     end
@@ -81,7 +99,9 @@ class UserOgpImageGenerator
     draw = Magick::Draw.new
 
     # ref: https://rmagick.github.io/draw.html#circle
-    draw.circle(size / 2, size / 2, size / 2, 0)
+    draw.stroke_width(5)
+    draw.stroke('gray50')
+    draw.circle(size / 2, size / 2, size / 2, 5)
     draw.draw(circle_image)
     mask = circle_image.blur_image(0, 1).negate
     mask.matte = false
